6aeb78ca3a614ebfd018703f88d202d86ca85a34
Antimo Angelino <aangelino@dpcontrol.it>
2018-07-05 15:52:18 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include "mbed.h"
+
+DigitalOut led1(LED1);
+DigitalIn start_in(D8,PullDown);
+PwmOut pwm_out_9(D9);
+Timer timer;
+
+Serial pc(SERIAL_TX, SERIAL_RX);
+
+int main()
+{
+
+    /*Info system clock*/
+    pc.printf("\n\rSystemCoreClock=%d\r\n",SystemCoreClock);
+
+    while(1)
+    {
+      /*to main retention, otherwise thread will be died*/
+      /*don't leave empty */
+      wait(1);
+    }
+
+    return 0;
+}
